43a2f949fa197e0655d7d575f0a93568eafe4892
Namrata <namratapatel015@gmail.com>
2021-08-20 18:36:27 +0530

    Last Update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
-import { MyserviceService } from '../myservice.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,17 +8,10 @@ import { MyserviceService } from '../myservice.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private myserv: MyserviceService) {
-    this.genre = this.myserv.genre
+  constructor() {
   }
   @Output('selected1') selected1: EventEmitter<any> = new EventEmitter<any>()
 
-  genre = 'Web Film'
-  selected = 'Genre'
-
-  changeGenre(val: string) {
-    this.selected1.emit(val);
-  }
 
   ngOnInit() {
   }
